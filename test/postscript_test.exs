2a15bdbd25f835dcc2dba08c7000b8e0850c873c
Jason Cartwright <jason@gomalomo.com>
2024-11-06 12:47:09 -0700

    fix: validate & log invalid JSON response

--- test/postscript_test.exs
+++ test/postscript_test.exs
@@ -1,20 +1,28 @@
 defmodule PostscriptTest do
   use ExUnit.Case, async: true
 
-  alias Postscript.{ Http, Operation, Response }
+  import ExUnit.CaptureLog, only: [with_log: 2]
 
-  @ok_resp %{ body: "{\"ok\":true}", headers: [], status_code: 200 }
+  alias Postscript.{Http, Operation, Response}
 
-  @not_ok_resp %{ body: "{\"ok\":false}", headers: [], status_code: 400 }
+  @ok_resp %{body: "{\"ok\":true}", headers: [], status_code: 200}
+
+  @not_ok_resp %{body: "{\"ok\":false}", headers: [], status_code: 400}
+
+  @not_json_resp %{
+    body: "not json",
+    headers: [{"content-type", "application/json"}],
+    status_code: 200
+  }
 
   test "sends the proper HTTP method" do
     Http.Mock.start_link()
 
-    response = { :ok, @ok_resp }
+    response = {:ok, @ok_resp}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :get, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :get, params: [hello: "world"], path: "/fake"}
 
     Postscript.request(operation, http_client: Http.Mock)
 
@@ -24,11 +32,11 @@ defmodule PostscriptTest do
   test "uses the proper URL for GET requests" do
     Http.Mock.start_link()
 
-    response = { :ok, @ok_resp }
+    response = {:ok, @ok_resp}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :get, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :get, params: [hello: "world"], path: "/fake"}
 
     Postscript.request(operation, http_client: Http.Mock)
 
@@ -38,11 +46,11 @@ defmodule PostscriptTest do
   test "uses the proper URL for DELETE requests" do
     Http.Mock.start_link()
 
-    response = { :ok, @ok_resp }
+    response = {:ok, @ok_resp}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :delete, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :delete, params: [hello: "world"], path: "/fake"}
 
     Postscript.request(operation, http_client: Http.Mock)
 
@@ -52,11 +60,11 @@ defmodule PostscriptTest do
   test "uses the proper URL for non-GET requests" do
     Http.Mock.start_link()
 
-    response = { :ok, @ok_resp }
+    response = {:ok, @ok_resp}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :post, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :post, params: [hello: "world"], path: "/fake"}
 
     Postscript.request(operation, http_client: Http.Mock)
 
@@ -66,31 +74,36 @@ defmodule PostscriptTest do
   test "sends the proper HTTP headers" do
     Http.Mock.start_link()
 
-    response = { :ok, @ok_resp }
+    response = {:ok, @ok_resp}
 
     Http.Mock.put_response(response)
 
     operation = %Operation{}
     operation = Map.put(operation, :method, :get)
-    operation = Map.put(operation, :params, [hello: "world"])
+    operation = Map.put(operation, :params, hello: "world")
     operation = Map.put(operation, :path, "/fake")
 
-    Postscript.request(operation, api_key: "thisisfake", http_client: Http.Mock, http_headers: [{ "x-custom-header", "true" }], shop_token: "thisisfake")
+    Postscript.request(operation,
+      api_key: "thisisfake",
+      http_client: Http.Mock,
+      http_headers: [{"x-custom-header", "true"}],
+      shop_token: "thisisfake"
+    )
 
-    assert { "content-type", "application/json" } in Http.Mock.get_request_headers()
-    assert { "authorization", "Bearer thisisfake" } in Http.Mock.get_request_headers()
-    assert { "x-custom-header", "true" } in Http.Mock.get_request_headers()
-    assert { "x-postscript-shop-token", "thisisfake" } in Http.Mock.get_request_headers()
+    assert {"content-type", "application/json"} in Http.Mock.get_request_headers()
+    assert {"authorization", "Bearer thisisfake"} in Http.Mock.get_request_headers()
+    assert {"x-custom-header", "true"} in Http.Mock.get_request_headers()
+    assert {"x-postscript-shop-token", "thisisfake"} in Http.Mock.get_request_headers()
   end
 
   test "sends the proper body for GET requests" do
     Http.Mock.start_link()
 
-    response = { :ok, @ok_resp }
+    response = {:ok, @ok_resp}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :get, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :get, params: [hello: "world"], path: "/fake"}
 
     Postscript.request(operation, http_client: Http.Mock)
 
@@ -100,11 +113,11 @@ defmodule PostscriptTest do
   test "sends the proper body for DELETE requests" do
     Http.Mock.start_link()
 
-    response = { :ok, @ok_resp }
+    response = {:ok, @ok_resp}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :delete, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :delete, params: [hello: "world"], path: "/fake"}
 
     Postscript.request(operation, http_client: Http.Mock)
 
@@ -114,11 +127,11 @@ defmodule PostscriptTest do
   test "sends the proper body for non-GET requests" do
     Http.Mock.start_link()
 
-    response = { :ok, @ok_resp }
+    response = {:ok, @ok_resp}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :post, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :post, params: [hello: "world"], path: "/fake"}
 
     Postscript.request(operation, http_client: Http.Mock)
 
@@ -128,39 +141,59 @@ defmodule PostscriptTest do
   test "returns :ok when the request is successful" do
     Http.Mock.start_link()
 
-    response = { :ok, @ok_resp }
+    response = {:ok, @ok_resp}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :post, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :post, params: [hello: "world"], path: "/fake"}
 
     result = Postscript.request(operation, http_client: Http.Mock)
 
-    assert { :ok, %Response{} } = result
+    assert {:ok, %Response{}} = result
   end
 
   test "returns :error when the request is not successful" do
     Http.Mock.start_link()
 
-    response = { :ok, @not_ok_resp }
+    response = {:ok, @not_ok_resp}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :post, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :post, params: [hello: "world"], path: "/fake"}
 
     result = Postscript.request(operation, http_client: Http.Mock)
 
-    assert { :error, %Response{} } = result
+    assert {:error, %Response{}} = result
+  end
+
+  test "logs warning and returns :error when response is not valid JSON" do
+    Http.Mock.start_link()
+
+    response = {:ok, @not_json_resp}
+
+    Http.Mock.put_response(response)
+
+    operation = %Operation{method: :post, params: [hello: "world"], path: "/fake"}
+
+    assert {result, log} =
+             with_log([level: :warning], fn ->
+               Postscript.request(operation, http_client: Http.Mock)
+             end)
+
+    assert {:error, _error} = result
+
+    assert log =~ "invalid JSON response"
+    assert log =~ "\"not json\""
   end
 
   test "passes the response through when unrecognized" do
     Http.Mock.start_link()
 
-    response = { :error, :timeout }
+    response = {:error, :timeout}
 
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :post, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :post, params: [hello: "world"], path: "/fake"}
 
     result = Postscript.request(operation, http_client: Http.Mock)
 
@@ -170,30 +203,30 @@ defmodule PostscriptTest do
   test "retries failed requests" do
     Http.Mock.start_link()
 
-    response_1 = { :error, :timeout }
-    response_2 = { :ok, @ok_resp }
+    response_1 = {:error, :timeout}
+    response_2 = {:ok, @ok_resp}
 
     Http.Mock.put_response(response_1)
     Http.Mock.put_response(response_2)
 
-    operation = %Operation{ method: :post, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :post, params: [hello: "world"], path: "/fake"}
 
     result = Postscript.request(operation, http_client: Http.Mock, retry: Postscript.Retry.Linear)
 
-    assert { :ok, %Response{} } = result
+    assert {:ok, %Response{}} = result
   end
 
   test "retries up to max attempts" do
     Http.Mock.start_link()
 
-    response = { :error, :timeout }
+    response = {:error, :timeout}
 
     Http.Mock.put_response(response)
     Http.Mock.put_response(response)
     Http.Mock.put_response(response)
     Http.Mock.put_response(response)
 
-    operation = %Operation{ method: :post, params: [hello: "world"], path: "/fake" }
+    operation = %Operation{method: :post, params: [hello: "world"], path: "/fake"}
 
     Postscript.request(operation, http_client: Http.Mock, retry: Postscript.Retry.Linear)
 
