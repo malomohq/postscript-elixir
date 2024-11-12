9c2252c548cd0ad39d0960153179ad7f39317e48
Anthony Smith <420061+anthonator@users.noreply.github.com>
2024-11-12 12:44:53 -0500

    chore: bumped version to 1.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Postscript.MixProject do
   def project do
     [
       app: :postscript,
-      version: "1.0.2",
+      version: "1.0.3",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
